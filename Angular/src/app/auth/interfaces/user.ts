d1800b42f190090757a036d824db96896bf7cd45
HMSBennett <harrisonsbennett@gmail.com>
2019-07-18 13:02:47 +0000

    remove all

--- Angular/src/app/auth/interfaces/user.ts
+++
@@ -1,5 +0,0 @@
-export interface IUser {
-    username: string,
-    firstName: string,
-    surname: string
-}
--- Mongodb/dockerInstall.sh
+++
@@ -1,3 +0,0 @@
-sudo docker pull mongo
-
-docker run -d -p 27017:27017 --name mongodb mongo
